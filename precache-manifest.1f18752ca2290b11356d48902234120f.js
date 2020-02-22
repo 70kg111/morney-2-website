a0703fa097e3e4a50ca580fab3b59fa3742504f8
70kg111 <76104718@qq.com>
2020-02-22 14:13:57 +0800

    deploy

---
+++ precache-manifest.1f18752ca2290b11356d48902234120f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1026b5623cded0de80c",
+    "url": "/morney-2-website/css/app.343f9ffe.css"
+  },
+  {
+    "revision": "e45631731064d5ce0f168af33fa99e53",
+    "url": "/morney-2-website/index.html"
+  },
+  {
+    "revision": "e1026b5623cded0de80c",
+    "url": "/morney-2-website/js/app.aa4eb7c0.js"
+  },
+  {
+    "revision": "c7800c44a4607d76d6ce",
+    "url": "/morney-2-website/js/chunk-vendors.2d9d542e.js"
+  },
+  {
+    "revision": "9c4d919a959084fb7ddde5e85ff2cef6",
+    "url": "/morney-2-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-2-website/robots.txt"
+  }
+]);
